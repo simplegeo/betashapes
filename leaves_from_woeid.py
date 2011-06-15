ea641ea51e31644c04252777c8da5871931637a6
ansate <pintyesantos@gmail.com>
2011-06-15 01:08:08 +0000

    you can choose which types are valid for leaves

--- leaves_from_woeid.py
+++ leaves_from_woeid.py
@@ -13,11 +13,15 @@ import copy
 def main():
     woeid = sys.argv[1]
 
+    leaftype = "Suburb"
+    if len(sys.argv) > 2:
+        leaftype = "'%s'" % sys.argv[2]
+
     outfile = "leaves_%s.csv" % woeid
 
     childq = """select * from woe_places
             where parent_id = %s
-            and placetype in ('LocalAdmin','Suburb')"""
+            and placetype in ('County','LocalAdmin','Suburb')"""
 
     conn_string = "dbname='hood'"
     # get a connection, if a connect cannot be made an exception will be raised here
@@ -27,22 +31,25 @@ def main():
     search = set([woeid])
     leaves = set()
     names = {}
+    types = {}
     while len(search) > 0:
         curr_search = copy.copy(search)
         for woe in curr_search:
             search.remove(woe)
-            qry = childq % woe
+            qry = childq % (woe, leaftypes)
             cursor.execute(qry)
             if cursor.rowcount == 0:
-                leaves.add((names[woe],woe))
+                if types[woe] == leaftype:
+                    leaves.add((names[woe],types[woe],woe))
             for line in cursor:
                 names[line['woe_id']] = line['name']
+                types[line['woe_id']] = line['placetype']
                 search.add(line['woe_id'])
 
     conn.close()
 
     owriter = csv.writer(open(outfile, 'w'))
-    owriter.writerow(["name","woe_id"])
+    owriter.writerow(["name","type","woe_id"])
     for leaf in leaves:
         owriter.writerow(leaf)
 
